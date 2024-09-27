30630387a8d3955d3c9d079a921e4b17a4578ab1
Agniamaulida23 <agniamaulida23@gmail.com>
2024-09-27 17:20:38 +0700

    upppp1

--- js/script.js
+++ js/script.js
@@ -30,6 +30,16 @@ function validateMessage(){
     alert("Tidak boleh kosong!");
     return false;
   }
+
+  setSenderUI(name, birthdate, gender, messages)
+  return false;
+}
+
+function setSenderUI(name, birthdate, gender, messages){
+  document.getElementById("sender-full-name").innerHTML = name;
+  document.getElementById("sender-birth-date").innerHTML = birthdate;
+  document.getElementById("sender-gender").innerHTML = gander;
+  document.getElementById("sender-messages").innerHTML = messages;
 }
 
 replaceName();
